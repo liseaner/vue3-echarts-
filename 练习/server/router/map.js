dce09a46668b930274d8b0a4fc4b419560088cb5
cai <1791019369@qq.com>
2022-12-19 11:20:37 +0800

    2022.12.19

---
+++ 练习/server/router/map.js
@@ -0,0 +1,8 @@
+let express=require("express");
+let router=express.Router()
+let mapData=require("../mock/china.json")
+
+router.get("/data",(req,res)=>{
+    res.send({msg:"我是地图的路由地址",chinaData:mapData})
+})
+module.exports=router
